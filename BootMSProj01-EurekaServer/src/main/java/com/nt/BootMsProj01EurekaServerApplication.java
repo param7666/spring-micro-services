ffcaf9fa18171206e4a98b7dbf30f599db142bea
Parmeshwar Gurlewad <114818920+param7666@users.noreply.github.com>
2025-10-16 23:15:05 +0530

    Add files via upload

---
+++ BootMSProj01-EurekaServer/src/main/java/com/nt/BootMsProj01EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BootMsProj01EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMsProj01EurekaServerApplication.class, args);
+		System.out.println("BootMsProj01EurekaServerApplication.main()");
+	}
+
+}
